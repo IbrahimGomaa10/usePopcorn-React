3df30c76ebe222e3e77210db362ed16a195c2c22
IbrahimGomaa10 <ibrahim.gomaa1013@gmail.com>
2023-12-30 16:39:29 +0200

    Changed http to https to avoid could't fetch mssg

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ export default function App() {
         setError("");
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`,
+          `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`,
           {
             signal: controller.signal,
           }
